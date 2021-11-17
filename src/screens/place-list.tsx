a5821c603b6308e7543b945eff90c99efb2ee2a6
Pavel Pochobradský <pavlik.pocho@gmail.com>
2021-11-17 00:33:19 +0000

    Search bar progress

--- src/screens/place-list.tsx
+++ src/screens/place-list.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react"
-import { View, Text, Button, ScrollView } from "react-native"
+import { View, Text, Button, ScrollView, TextInput, Keyboard } from "react-native"
 import PlaceOverview from "../components/place-overview";
 import RootStackParamList from "../root-stack-param-list/root-stack-param-list";
 
@@ -12,14 +12,28 @@ interface PlaceListProps extends PropType {
 
 const PlaceList: React.FC<PlaceListProps> = ({ navigation, route, ...props }) => {
 
+    const [searching, setSearching] = React.useState(false);
+
+    const getSideButton = () => {
+        return !searching ? <Button title="Profile" onPress={() => {}} key='profile-button' /> : <Button title="Cancel" onPress={() => {Keyboard.dismiss()}} key={'cancel-button'} />
+    }
+
+    // This is very broken, because a state change does not trigger a new call of the useLayoutEffect
+
     React.useLayoutEffect(() => {
 		navigation.setOptions({
             header: () => (
                 <View style={{ flexDirection: 'row', marginTop: 28, height: 40 }}>
                     <View style={{ flex: 1, height: 24, backgroundColor: "black" }}>
-                        <Text style={{ color: 'white' }}>Search bar here</Text>
+                        <TextInput style={{ color: 'white' }} onFocus={() => {
+                            console.log("Setting searching true");
+                            setSearching(true);
+                        }} onBlur={() => {
+                            console.log("Setting searching false");
+                            setSearching(false);
+                        }}/>
                     </View>
-                    <Button title="Profile" onPress={() => {}} />
+                    {getSideButton()}
                 </View>
             )
             // headerLeft: () => {
